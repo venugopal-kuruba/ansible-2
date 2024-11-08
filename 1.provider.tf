6e4e730dab8b1e3a3d3608f534692b4587db3922
kuruba venu gopal <kurubavenugopal1435@gmail.com>
2024-11-08 14:45:55 +0530

    ansible code for deployment

---
+++ 1.provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_version = "<= 1.9.8"
+  required_providers {
+    aws = {
+      version = "<= 5.75.0"
+      source  = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket = "kuruba.xyz"
+    key    = "ansible-kuruba.tfstate"
+    region = "us-east-1"
+  }
+}
